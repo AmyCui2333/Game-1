ca617fa61eb71b36dfbd5b9cb7540f4b1e118d74
cs897 <cs897@cornell.edu>
2020-10-24 03:31:51 -0400

    I know, I know, this is not how I’m supposed to do it, but I can't think of something better.

--- src/main/java/Controller/ControllerImpl.java
+++ src/main/java/Controller/ControllerImpl.java
@@ -1,28 +1,43 @@
 package Controller;
 
 import Model.Model;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
+import javafx.util.Duration;
+
 
 
 public class ControllerImpl extends Controller
 {
-    private Image play, pause;
-    private Button playPause;
+    private final Image play, pause, step;
+    private final Button playPause;
+    private final Button stepBtn;
+    private final Model model;
+    private final Timeline gogogo;
+    private double secondsPerDay;
     public ControllerImpl(Model m)
     {
+        model = m;
+        secondsPerDay = 1;
         VBox mainV = new VBox();
         Label title = new Label("Controls");
         title.setFont(Font.font(30));
         playing = false;
-        play = new Image(getClass().getResourceAsStream("play.png"), 30, 30, false, false);
-        pause = new Image(getClass().getResourceAsStream("pause.png"), 30, 30, false, false);
+        play = new Image(getClass().getResourceAsStream("play.png"), 30, 30, false, true);
+        pause = new Image(getClass().getResourceAsStream("pause.png"), 30, 30, false, true);
+        step = new Image(getClass().getResourceAsStream("step.png"), 30, 30, false, true);
         playPause = new Button();
         playPause.setGraphic(new ImageView(play));
         playPause.setOnAction(e ->
@@ -32,9 +47,34 @@ public class ControllerImpl extends Controller
                 playPause.setGraphic(new ImageView(pause));
             else playPause.setGraphic(new ImageView(play));
         });
+        stepBtn = new Button();
+        stepBtn.setGraphic(new ImageView(step));
+        stepBtn.setOnAction(e ->
+        {
+            model.dayPass();
+        });
+        gogogo = new Timeline(
+                new KeyFrame(Duration.seconds(secondsPerDay), a ->
+                        {
+                            if(playing)
+                            {
+                                //TODO ENABLE synchronized (model)
+                                {
+                                    System.out.println("moving");
+                                    //model.dayPass();
+                                }
+                            }
+                        }));
+        gogogo.setCycleCount(Timeline.INDEFINITE);
+        gogogo.play();
+        gogogo.setRate(10);
         mainV.setPadding(new Insets(20));
         mainV.setSpacing(20);
-        mainV.getChildren().addAll(title, playPause, new Separator());
+        HBox buttonBox = new HBox();
+        buttonBox.setSpacing(30);
+        buttonBox.setAlignment(Pos.CENTER);
+        buttonBox.getChildren().addAll(stepBtn, playPause);
+        mainV.getChildren().addAll(title, buttonBox, new Separator());
         this.getChildren().add(mainV);
     }
 }
