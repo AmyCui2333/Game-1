1e96ec095b9588c012b3083c314366170ad0a95f
cs897 <cs897@cornell.edu>
2020-10-24 15:53:47 -0400

    Still can't get this right...

--- src/main/java/Controller/ControllerFac.java
+++ src/main/java/Controller/ControllerFac.java
@@ -1,11 +1,12 @@
 package Controller;
 
 import Model.Model;
+import View.View;
 
 public class ControllerFac
 {
-    public static Controller getCont(Model m)
+    public static Controller getCont(Model m, View v)
     {
-        return new ControllerImpl(m);
+        return new ControllerImpl(m, v);
     }
 }
