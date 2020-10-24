ccbfcc03fb3df2710c0ca5e719d4676dd0d30308
Amy <504126159@qq.com>
2020-10-24 01:35:13 -0400

    model interface

--- src/main/java/Model/Model.java
+++ src/main/java/Model/Model.java
@@ -1,5 +1,30 @@
 package Model;
 
-public class Model
-{
+import java.util.ArrayList;
+
+public interface Model {
+    int day = 0;
+
+    ArrayList<NodeImplementation> getNodeinRange(double minx, double maxx, double miny, double maxy);
+
+    int getPopulation(ArrayList nodeList);
+
+    int getInfectedTotal(ArrayList nodeList);
+
+    int getInfectedLoc(NodeImplementation node);
+
+    boolean getMasked(NodeImplementation node);
+
+    void setMasked(NodeImplementation node);
+
+    boolean getShutDown(NodeImplementation node);
+
+    void setShutDown(NodeImplementation node);
+
+    int getShutDownTotal(ArrayList nodeList);
+
+    int getDay();
+
+    void skipDay();
+
 }
---
+++ src/main/java/Model/NodeImplementation.java
@@ -0,0 +1,13 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class NodeImplementation {
+    private double xloc;
+    private double yloc;
+    private int population;
+    private int infected;
+    private ArrayList<NodeImplementation> connected;
+    private boolean masked;
+    private boolean shutdown;
+}
